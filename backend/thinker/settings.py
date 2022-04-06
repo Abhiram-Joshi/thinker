5281e20f0a0e5adc6d04a19c62e2d482944a1e4e
Abhiram <abramjoshi@gmail.com>
2022-04-06 11:13:53 +0530

    fix: heroku config files

--- backend/thinker/settings.py
+++ backend/thinker/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["thinkerr.herokuapp.com"]
 
 AUTH_USER_MODEL = "account.User"
 
