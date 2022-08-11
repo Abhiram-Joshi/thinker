d8fc2bc4259bccb3e109ef8be9838f880b9e00cd
Abhiram <abramjoshi@gmail.com>
2022-08-11 14:26:24 +0530

    fix: replaced uuid with username and user photo url in deck information response

--- backend/deck/serializers.py
+++ backend/deck/serializers.py
@@ -15,10 +15,12 @@ class UpdateDeckSerializer(serializers.ModelSerializer):
 
 class DeckSerializer(serializers.ModelSerializer):
     bookmarked = serializers.SerializerMethodField()
+    user_name = serializers.SerializerMethodField()
+    user_photo_url = serializers.SerializerMethodField()
 
     class Meta:
         model = Deck
-        exclude = ["reported", "bookmarked_by",]
+        exclude = ["user", "reported", "bookmarked_by",]
 
     def get_bookmarked(self, obj):
         if self.context.get('request').user in obj.bookmarked_by.all():
@@ -26,3 +28,9 @@ class DeckSerializer(serializers.ModelSerializer):
         
         else:
             return False
+
+    def get_user_name(self, obj):
+        return obj.user.name
+    
+    def get_user_photo_url(self, obj):
+        return obj.user.photo_url
