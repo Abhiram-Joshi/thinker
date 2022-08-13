b00dc844deb24fc2511ad737b6e12c5d952e1052
Abhiram <abramjoshi@gmail.com>
2022-08-13 16:15:03 +0530

    fix: returning more randomised facts from wikipedia

--- backend/deck/utilities.py
+++ backend/deck/utilities.py
@@ -1,3 +1,5 @@
+import random
+import re
 import wikipedia
 
 def get_facts(topic: str) -> list:
@@ -6,10 +8,16 @@ def get_facts(topic: str) -> list:
         wikipedia.set_lang("en")
         valid_topic = wikipedia.search(topic)[0]
 
-        summary = wikipedia.summary(valid_topic, sentences=5)
+        summary = wikipedia.summary(valid_topic, sentences=50)
+
+        summary_wo_headings = re.sub(r"==.*==", "", summary)
+        summary_wo_blanks = re.sub(r"\n", "", summary_wo_headings)
         
         facts = summary.split(". ")
-        return facts
+
+        random_indices = [random.randint(0, len(facts)-1) for i in range(5)]
+
+        return [facts[i] for i in random_indices]
 
     except wikipedia.exceptions.DisambiguationError as e:
         print(e)
