448bcac300efd2bff7eb7e196edfeca55693b114
Abhiram <abramjoshi@gmail.com>
2022-08-13 15:03:44 +0530

    fix: added max length to uuid attribute of user model

--- backend/account/models.py
+++ backend/account/models.py
@@ -9,7 +9,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     name = models.CharField(max_length=20, unique=True)
     email = models.EmailField(unique=True)
     photo_url = models.URLField()
-    uuid = models.CharField(unique=True, primary_key=True)
+    uuid = models.CharField(max_length=50, unique=True, primary_key=True)
     bio = models.CharField(max_length=255, blank=True)
     is_active = models.BooleanField(default=True)
 
