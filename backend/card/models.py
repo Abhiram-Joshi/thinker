d180d133948e622b575d82cf74c729a5ae7f7131
Abhiram <abramjoshi@gmail.com>
2022-08-19 12:41:15 +0530

    fix: increased character limit for card content from 100 to 255

--- backend/card/models.py
+++ backend/card/models.py
@@ -3,5 +3,5 @@ from deck.models import Deck
 
 # Create your models here.
 class Card(models.Model):
-    content = models.CharField(max_length=100)
+    content = models.CharField(max_length=255)
     deck = models.ForeignKey(Deck, on_delete=models.CASCADE)
